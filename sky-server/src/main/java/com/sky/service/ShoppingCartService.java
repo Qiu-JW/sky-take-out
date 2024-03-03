783af0ccd4b7453bfeb67faa4a563ab1b4eaa164
QiuJW <2058148675@qq.com>
2024-03-03 10:14:24 +0800

    减少商品

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -13,16 +13,20 @@ public interface ShoppingCartService {
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
 
-
     /**
-     * 查看购物车
+     *  查看购物车中的内容
      * @return
      */
     List<ShoppingCart> showShoppingCart();
+
     /**
-     * 清空购物车
+     * 清空购物车中的商品
      */
     void cleanShoppingCart();
 
-
+    /**
+     * 删除购物车中的东西
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
